e12355e2cfa92f3a7bd7ed430cdff29b46019381
Antonio Mérida <131478083+Atheb3003@users.noreply.github.com>
2023-10-05 18:44:30 +0200

    Delete Introduccion directory

--- Introduccion/1_HolaMundo.py
+++
@@ -1,3 +0,0 @@
-print('Hola Mundo')
-# Python no utiliza el punto y coma (;) 
-# Puede ponerse tanto comilla simple como doble ("") o ('')
--- Introduccion/2_Variables.py
+++
@@ -1,9 +0,0 @@
-texto = "Papaya"
-numero_papayas = 67
-precio_papayas = 9.999999
-print (type(texto))
-print (texto)
-print (type(numero_papayas))
-print (numero_papayas)
-print (type(precio_papayas))
-print (precio_papayas)
