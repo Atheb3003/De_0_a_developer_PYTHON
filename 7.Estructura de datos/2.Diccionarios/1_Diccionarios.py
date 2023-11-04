2d35cccd32ccb35ca1958c5d20d925c1099a863f
Atheb3003 <antoniomerida3003@outlook.es>
2023-11-04 12:02:51 +0100

    Creacion y modificacion de diccionarios

---
+++ 7.Estructura de datos/2.Diccionarios/1_Diccionarios.py
@@ -0,0 +1,12 @@
+diccionario = {"nombre" : "Pepito", "apellido" : "Jimenez", "estatura" : 1.92}
+print(diccionario) #IMPRIME EL DICCIONARIO EN SU TOTALIUDAD
+
+print(diccionario.keys()) #IMPRIME UNICAMENTE LAS LLAVES / CLAVES DEL DICCIONARIO
+
+print(diccionario.values()) #IMPRIME LOS VALORES DEL DICCIONARIO
+
+print(diccionario["apellido"]) #IMPRIME EL VALOR CORRESPONDIENTE EN A LA CLAVE ESCRITA EN EL []
+
+diccionario["edad"] = 63 #AÑADIR NUEVOS ELEMENTOS A UN DICCIONARIO, AS'I TAMBIEN PUEDEN MODIFICARSE ELEMENTOS YA EXISTENTES
+
+print(diccionario["edad"])
